50415d1ea07a95bf1790801bb027f40b25a0a81e
Milan Varasada <milan.varasada@solutionanalysts.com>
2020-08-17 15:19:09 +0530

    Product Design
    [01] Simple Product design done,
    
    State management by provider is left.

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,46 @@
+import 'package:MyShop_Flutter/screens/product_detail_screen.dart';
+import 'package:flutter/material.dart';
+import '../screens/product_detail_screen.dart';
+
+class ProductItem extends StatelessWidget {
+  final String title;
+  final String id;
+  final String imageUrl;
+
+  ProductItem(this.id, this.title, this.imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context)
+                .pushNamed(ProductDetailScreen.routName, arguments: id);
+          },
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            icon: Icon(Icons.favorite),
+            color: Theme.of(context).accentColor,
+            onPressed: () {},
+          ),
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+              icon: Icon(Icons.shopping_cart),
+              color: Theme.of(context).accentColor,
+              onPressed: () {}),
+        ),
+      ),
+    );
+  }
+}
